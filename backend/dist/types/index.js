ed4376f03aa1009eefcd8dd4aef5dd5502be9ce7
Juan Cruz Benitez <empresadejj@gmail.com>
2026-06-01 00:52:59 -0300

    chore: ignora backend/dist del repositorio

--- backend/dist/types/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.parseId = void 0;
-/** Parsea un param de ruta Express (string | string[]) a number */
-const parseId = (param) => parseInt(Array.isArray(param) ? param[0] : param, 10);
-exports.parseId = parseId;
